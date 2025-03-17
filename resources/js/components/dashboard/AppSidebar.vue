9d8736447f7fa76b24cd2352db331eff9fc3802c
iqbalmusyaffa <yusa82401@gmail.com>
2025-03-17 23:52:14 +0700

    update logo

--- resources/js/components/dashboard/AppSidebar.vue
+++ resources/js/components/dashboard/AppSidebar.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { RouterLink } from 'vue-router'
-
-import { logo } from '@/assets/brand/logo'
-import { sygnet } from '@/assets/brand/sygnet'
+import logo from '@/assets/logo/logo.png' // Pastikan path ini sesuai dengan lokasi file
 import { AppSidebarNav } from '@/components/dashboard/AppSidebarNav.js'
 import { useSidebarStore } from '@/stores/sidebar.js'
 
@@ -21,13 +19,14 @@ const sidebar = useSidebarStore()
     <CSidebarHeader class="border-bottom">
       <RouterLink custom to="/" v-slot="{ href, navigate }">
         <CSidebarBrand v-bind="$attrs" as="a" :href="href" @click="navigate">
-          <CIcon custom-class-name="sidebar-brand-full" :icon="logo" :height="32" />
-          <CIcon custom-class-name="sidebar-brand-narrow" :icon="sygnet" :height="32" />
+          <img :src="logo" alt="Logo" height="45" class="sidebar-brand-full" />
         </CSidebarBrand>
       </RouterLink>
       <CCloseButton class="d-lg-none" dark @click="sidebar.toggleVisible()" />
     </CSidebarHeader>
+
     <AppSidebarNav />
+
     <CSidebarFooter class="border-top d-none d-lg-flex">
       <CSidebarToggler @click="sidebar.toggleUnfoldable()" />
     </CSidebarFooter>
